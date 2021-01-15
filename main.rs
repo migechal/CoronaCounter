169af2501d69ea9ffce49c4b553ca1c4232f885f
Mikhail Chalakov <mikhail@chalakov.net>
2021-01-14 22:20:37 -0800

    added files and stuff

---
+++ main.rs
@@ -0,0 +1,36 @@
+
+struct FileDo;
+impl FileDo{
+    fn writeToFile(file: String, text: String){
+        use std::io::Write;
+        let mut file = std::fs::File::create(file).expect("create failed");
+        file.write_all(text.as_bytes()).expect("write failed");
+    }
+    fn readFromFile(file: String) -> String{
+        use std::io::Read;
+        let mut File = std::fs::File::open(file).unwrap(); 
+        let mut contents = String::new();
+        File.read_to_string(&mut contents).unwrap();
+        return contents;
+    }
+    fn delete(file: String){
+        use std::fs;
+        fs::remove_file(file).expect("could not remove file");
+    }
+}
+use xlsxwriter::{DateTime as XLSDateTime, Format, Workbook, Worksheet};
+fn seekExcel(sheet: &mut Worksheet, state: String){
+
+}
+
+
+fn main(){
+    use std::io::{stdout, Write, Read};
+    use curl::easy::Easy;
+    let mut covid = Easy::new();
+    covid.url("https://www.rust-lang.org/").unwrap();
+    covid.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+}   
