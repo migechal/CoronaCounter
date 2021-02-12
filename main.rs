04747b0a8c77b05624c0071ad9f4bc95b818805c
Mikhail Chalakov <mikhail@chalakov.net>
2021-02-11 23:47:01 -0800

    updated

--- main.rs
+++ main.rs
@@ -1,79 +1,82 @@
-#![allow(non_snake_case)]
+use curl::easy::Easy;
+use std::fs::{remove_file, File};
+use std::io::{prelude::*, stdin, ErrorKind};
+use std::str;
 
-use error_chain::error_chain;
-use std::path::Path;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::stdin;
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
-}
+struct FileDo {}
 
-struct FileDo;
-impl FileDo{
-    fn write_to_file(file: String, text: String){
-        let mut file = std::fs::File::create(file).expect("create failed");
-        file.write_all(text.as_bytes()).expect("write failed");
+impl FileDo {
+    fn write_to_file(file: String, text: String) {
+        let mut file = std::fs::File::create(file).expect("Failed to open file");
+        file.write_all(text.as_bytes()).expect("Write failed");
     }
-    fn read_from_file(file: String) -> String{
-        let mut file = std::fs::File::open(file).unwrap(); 
+    fn read_from_file(file: String) -> String {
+        let mut file = std::fs::File::open(file).unwrap();
         let mut contents = String::new();
         file.read_to_string(&mut contents).unwrap();
         return contents;
     }
-    fn delete(file: String){
-        use std::fs;
-        fs::remove_file(file).expect("could not remove file");
+    fn _delete_file(file: String) {
+        remove_file(file).expect("Could not remove file");
     }
-    fn create_if_not_exist(file: String) -> u8{
-        use std::fs;
-        if !std::path::Path::new(&file).exists(){
-            fs::File::create(file).expect("Failed to create file");
-            return 1;
-        }
-        else{
-            return 0;
+    fn create_if_not_exist(file: String) -> u8 {
+        if !std::path::Path::new(&file).exists() {
+            File::create(file).expect("Failed to create file");
+            1
+        } else {
+            0
         }
     }
+    // fn is_empty_file(file: String) -> bool {
+    //     use std::fs;
+    //     if !std::path::Path(&file).as_bytes() {
+    //         fs::File::create(file).expect("Failed to create file");
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // }
 }
 
-
-
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let _comma: i8 = 21;
-
-    if FileDo::create_if_not_exist("state.txt".to_owned()) == 1{
+fn main() -> std::io::Result<()> {
+    if FileDo::create_if_not_exist("./state.txt".to_owned()) == 1 {
         println!("Enter your state name:  ");
+
         let mut state_name = String::new();
         stdin().read_line(&mut state_name).expect("Not a string");
-        FileDo::write_to_file("state.txt".to_owned(), state_name.to_lowercase());
+
+        FileDo::write_to_file("./state.txt".to_owned(), state_name.to_lowercase());
     }
 
-    
-    let state = FileDo::read_from_file("state.txt".to_owned()).to_lowercase();
+    let state = FileDo::read_from_file("./state.txt".to_owned()).to_lowercase();
 
-    let target = "https://covidtracking.com/data/download/".to_string() + &state + "-history.csv";
+    let path = "./cases.csv";
 
-    println!("{}", target);
+    let mut covid = Easy::new();
 
-    let response = reqwest::get(&target).await?;
+    covid
+        .url(&("https://covidtracking.com/data/download/".to_string() + &state + "-history.csv"))
+        .unwrap();
 
-    let path = Path::new("./cases.csv");
+    let mut data_from_web: String = String::new();
+    {
+        let mut transfer_data = covid.transfer();
+        transfer_data
+            .write_function(|data| {
+                data_from_web.push_str(str::from_utf8(data).unwrap());
+                Ok(data.len())
+            })
+            .unwrap();
+        transfer_data.perform().unwrap();
+    }
 
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}", why),
-        Ok(file) => file,
-    };
-    
-    let content =  response.text().await?;
-    file.write_all(content.as_bytes())?;
-    
-    println!("{}", FileDo::read_from_file("./cases.csv".to_string()));
+    FileDo::write_to_file(path.to_string(), data_from_web);
 
-    Ok(())
+    if std::path::Path::new(&path).exists() {
+        println!("{}", FileDo::read_from_file("./cases.csv".to_string()));
+        Ok(())
+    } else {
+        //        println!("Error! File {} not found. (This issue is most likely not caused by you, please report it at https://github.com/migechal/CoronaCounter/issues", path);
+        Err(std::io::Error::new(ErrorKind::NotFound, path))
+    }
 }
