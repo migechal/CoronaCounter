51f250f988a719e8fb483c7a8b6c2ebe84e0b65f
Mikhail Chalakov <mikhail@chalakov.net>
2021-06-24 10:04:57 -0700

    I actually made it work

--- install_deps.sh
+++ install_deps.sh
@@ -5,3 +5,6 @@ elif [ -x "$(command -v dnf)" ];     then sudo dnf install R cargo
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install R-base-devel cargo
 elif [ -x "$(command -v pacman)" ]; then sudo pacman -S r cargo
 else echo "Err: Package manager not found... You must manually install: R, and the package cargo">&2; fi
+
+rustup install stable
+rustup default stable
--- main.rs
+++ main.rs
@@ -1,6 +1,6 @@
 use curl::easy::Easy;
 use run_script::ScriptOptions;
-use std::fs::{remove_file, File};
+use std::fs::remove_file;
 use std::io::{prelude::*, stdin, ErrorKind};
 use std::str;
 
@@ -27,38 +27,39 @@ impl FileDo {
         remove_file(file).expect("Could not remove file");
     }
     fn create_if_not_exist(file: String) -> u8 {
-        if !FileDo::exists(file) {
-            File::create(file).expect("Failed to create file");
+        let file_tmp = &file;
+        if !FileDo::exists(file_tmp.to_string()) {
+            std::fs::File::create(file).expect("Failed to create file");
             1
         } else {
             0
         }
     }
-    // fn is_empty_file(file: String) -> bool {
-    //     use std::fs;
-    //     if !std::path::Path(&file).as_bytes() {
-    //         fs::File::create(file).expect("Failed to create file");
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // }
 }
 
 fn main() -> std::io::Result<()> {
+    let new_state: String = String::from('n');
+    let get_covid_from_past: String = String::from('f');
     let file_name = "./state.txt".to_string();
     let beg_text: String = if FileDo::exists(file_name.to_owned()) {
-        "Find covid press (f)".to_string()
+        let mut owned_string: String =
+            "To find Covid-19 cases from your past entry--press (".to_owned();
+        owned_string.push_str(&get_covid_from_past);
+        owned_string.push(')');
+        owned_string.to_owned()
     } else {
-        "".to_string()
+        " ".to_string()
     };
-    println!("{} | Press {} to enter new state:  ", beg_text, "n");
-
     let mut opt: String = String::new();
-
-    stdin().read_line(&mut opt).expect("Not a char");
-
-    if FileDo::create_if_not_exist(file_name.to_owned()) == 1 || opt == "n" {
+    while opt != new_state && opt != get_covid_from_past {
+        println!("{} | Press ({}) to enter new state:  ", beg_text, new_state);
+        opt.clear();
+        stdin().read_line(&mut opt).expect("Not a char");
+        opt = opt.trim().to_string();
+        // println!("{:?} is what you entered and {:?} is next", opt, new_state);
+    }
+    if FileDo::create_if_not_exist(file_name.to_owned()) == 1 || opt == new_state {
+        FileDo::delete_file("./state.txt".to_owned());
         println!("Enter your state name:  ");
 
         let mut state_name = String::new();
@@ -92,22 +93,27 @@ fn main() -> std::io::Result<()> {
     FileDo::write_to_file(path.to_string(), data_from_web);
 
     let options = ScriptOptions::new();
-
     let args = vec![];
-
+    print!("\x1B[2J\x1B[1;1H");
     if std::path::Path::new(&path).exists() {
         let (_code, output, _error) = run_script::run(
             r#"
             Rscript GetData.R
-           "#,
+            "#,
             &args,
             &options,
         )
         .unwrap();
-        println!("Their are: {} cases in the state of {}", output, state);
+        if output == "" {
+            println!("What you entered as your state is probably not a state, if I'm wrong, let me know and report it at https://github.com/migechal/CoronaCounter/issues :)");
+        } else {
+            println!("Their are: {} cases in the state of {}", output, state);
+        }
+        FileDo::delete_file("./cases.csv".to_owned());
         Ok(())
     } else {
         println!("Error! File {} not found. (This issue is most likely not caused by you, please report it at https://github.com/migechal/CoronaCounter/issues", path);
+        FileDo::delete_file("./cases.csv".to_owned());
         Err(std::io::Error::new(ErrorKind::NotFound, path))
     }
 }
